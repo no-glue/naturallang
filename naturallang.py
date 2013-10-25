04434f12f7f2c2eb5139910a34b37a507597524d
Nikola Pavlicevic <nikola_pav@eunet.rs>
2013-10-25 21:25:22 +0200

    plots

--- naturallang.py
+++ naturallang.py
@@ -21,3 +21,23 @@ def gutenberg_stats(gutenberg):
     num_vocab = len(set(gutenberg.words(fileid)))
     
     print int(num_chars/num_words), int(num_words/num_sents), int(num_words/num_vocab), fileid
+
+def conditional_freq(nltk, words_list, text):
+  """Gets conditional frequency of words
+  
+  """
+  return nltk.ConditionalFreqDist(
+    (target, fileid[:4])
+    for fileid in text.fileids()
+    for w in text.words(fileid)
+    for target in words_list
+    if w.lower().startswith(target))
+
+def cumulative_word_length(nltk, languages, text):
+  """Gets cumulative lengths for text
+  
+  """
+  return nltk.ConditionalFreqDist(
+    (lang, len(word))
+    for lang in languages
+    for word in text.words(lang + '-Latin1'))
